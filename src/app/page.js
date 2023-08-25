de0fecccb90560567735b942f6c691b2453aa863
aungmyomyat220 <aungmyomyat874@gmail.com>
2023-08-25 11:28:33 +0630

    Bloging Project with NextJs and TailWind CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,94 @@
 import Image from 'next/image'
-
+import Cover from '../image/cover (1).jpg'
+import JJLogo from '../image/jj.png'
+import Cover2 from '../image/cover (2).jpg'
+import Cover3 from '../image/cover (3).jpg'
+import Author1 from '../image/joe.jpeg'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className='flex flex-col w-full justify-center items-center'>
+          <div className='max-w-5xl w-full'>
+          <div className='flex flex-col items-center mt-20 sm:flex-row sm:justify-between sm:pl-3'>
+              <span className='text-4xl font-bold'>Blog.</span>
+              <span className='px-6 mt-3 text-center'>A statically generated blog example using <a href='https://nextjs.org/' className='underline'>Next.js</a> and Markdown</span>
+          </div>
+          <div className='w-full px-4 mt-12'>
+             <Image src={Cover2} alt='' className='h-52 md:h-fit'/>
+          </div>
+          <div className='sm:grid sm:grid-cols-2 mt-8'>
+              <div className='px-4 flex flex-col sm:mt-3'>
+                  <span className='text-2xl font-semibold mb-3'>Dynamic Routing and Static Generation</span>
+                  <span>March 16, 2020</span>
+              </div>
+              <div className='px-4 mt-5 flex flex-col'>
+                  <span className=''>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.
+                  </span>
+                  <div className="mt-2 flex">
+                      <Image src={JJLogo} alt='' className='w-12 h-12'></Image>
+                      <span className='font-bold text-lg ml-2 mt-2'>JJ Kasper</span>
+                  </div>
+              </div>
+          </div>
+
+
+          {/*More Stories*/}
+          <div className='flex flex-col items-start mt-14 px-3'>
+              <span className='text-4xl font-bold mb-5'>More Stories</span>
+          </div>
+
+          <div className='md:grid md:grid-cols-2'>
+          {/*News Div*/}
+          <div className='mt-8 md:mr-4'>
+              <Image src={Cover3} alt='' className='h-52 mb-2 px-3'/>
+              <div className='sm:grid sm:grid-cols-2'>
+                  <div className='px-4 flex flex-col sm:mt-3'>
+                      <span className='text-2xl font-semibold mb-3'>Learn How to Pre-render Pages Using Static Generation with Next.js</span>
+                      <span>March 16, 2020</span>
+                  </div>
+                  <div className='px-4 mt-5 flex flex-col'>
+                  <span>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.
+                  </span>
+                      <div className="mt-3 flex">
+                          <Image src={Author1} alt='' className='w-12 h-12 rounded-full'></Image>
+                          <span className='font-bold text-lg ml-2 mt-2'>Angela Gray</span>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+          {/*News Div*/}
+          <div className='mt-8 pb-32 md:ml-4'>
+              <Image src={Cover} alt='' className='h-52 mb-2 px-3'/>
+              <div className='sm:grid sm:grid-cols-2'>
+                  <div className='px-4 flex flex-col sm:mt-3'>
+                      <span className='text-2xl font-semibold mb-3'>Preview Mode for Static Generation</span>
+                      <span>March 16, 2020</span>
+                  </div>
+                  <div className='px-4 mt-5 flex flex-col'>
+                  <span>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.
+                  </span>
+                      <div className="mt-3 flex">
+                          <Image src={Author1} alt='' className='w-12 h-12 rounded-full'></Image>
+                          <span className='font-bold text-lg ml-2 mt-2'>Joe</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          </div>
+
+          {/*Footer*/}
+          <div className='bg-gray-200 flex flex-col  md:grid md:grid-cols-6 justify-center items-center text-center px-3 sm:px-2 py-20'>
+            <span className='font-bold text-3xl col-span-3 mr-3'>Statically Generated with Next.js.</span>
+              <button className='bg-black px-10 py-3 sm:mx-10 border col-span-2 text-white my-8 font-medium hover:bg-white hover:text-black hover:border-black'><a href="https://nextjs.org/">Read Documentation</a></button>
+            <span className='font-bold'><a href='https://github.com/aungmyomyat4980/BlogFridayTest.git'>View on GitHub</a></span>
+          </div>
+          </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
---
+++ src/app/posts/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const post = () => {
+    return (
+        <div>
+            Hello Word
+        </div>
+    );
+};
+
+export default post;
